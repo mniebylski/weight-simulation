3f872693ba149ce5554488d2c49e614de24049dc
mniebylski <mniebylski@128.231.83.58>
2015-06-17 10:12:27 -0400

    Update

--- WeightSimulation/src/weightsimulation/Display.java
+++ WeightSimulation/src/weightsimulation/Display.java
@@ -7,30 +7,44 @@ package weightsimulation;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
 
 public class Display {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				javax.swing.JFrame frame = new JFrame("Charts");
-				System.out.println();
-				frame.setSize(600, 400);
+				javax.swing.JFrame frame = new JFrame("Weight Simulation");
+
+				frame.setSize(800, 600);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
-				
-				XYDataset ds = createDataset();
-				JFreeChart chart = ChartFactory.createXYLineChart("Test Chart",
-						"x", "y", ds, PlotOrientation.VERTICAL, true, true,
-						false);
 
+				XYDataset ds = createDataset();
+				JFreeChart chart = ChartFactory.createTimeSeriesChart(
+						"Weight Over Time", "Date (mm/dd/yy)", "Weight (lbs)",
+						ds);
+
+				
+				XYPlot plot = (XYPlot) chart.getPlot();
+				
+				//Set Custom Range
+				ValueAxis yAxis = plot.getRangeAxis();
+				yAxis.setRange(100.0, 180.0);
+				
+				//Set Custom Domain
+				ValueAxis xAxis = plot.getDomainAxis();
+				xAxis.setRange(0,5);
+				
 				ChartPanel cp = new ChartPanel(chart);
 
 				frame.getContentPane().add(cp);
@@ -38,12 +52,14 @@ public class Display {
 		});
 
 	}
-	
+
+	// Generate Points For Graph
 	private static XYDataset createDataset() {
 
 		DefaultXYDataset ds = new DefaultXYDataset();
 
-		double[][] data = { { 0.1, 0.2, 0.3 }, { 1, 2, 3 } };
+		// Dates Row 1, Weights Row 2
+		double[][] data = { { 0, 1, 2, 3 }, { 160, 159, 160, 156 } };
 
 		ds.addSeries("series1", data);
 
