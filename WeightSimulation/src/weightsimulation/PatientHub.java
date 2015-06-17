180a50ba9dd4bb4e03953818887eaf6f87a6d394
mniebylski <mniebylski@128.231.83.58>
2015-06-17 16:00:03 -0400

    created

---
+++ WeightSimulation/src/weightsimulation/PatientHub.java
@@ -0,0 +1,46 @@
+package weightsimulation;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class PatientHub extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PatientHub frame = new PatientHub();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PatientHub() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 355, 345);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblWeightLossHub = new JLabel("Weight Loss Hub");
+		lblWeightLossHub.setBounds(126, 6, 106, 33);
+		contentPane.add(lblWeightLossHub);
+	}
+}
