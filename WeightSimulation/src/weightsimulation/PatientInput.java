e521f074d254c0a36eaef95d2eb9ff9a061c120b
mniebylski <mniebylski@128.231.83.58>
2015-06-17 12:22:08 -0400

    added

---
+++ WeightSimulation/src/weightsimulation/PatientInput.java
@@ -0,0 +1,46 @@
+package weightsimulation;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class PatientInput extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PatientInput frame = new PatientInput();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PatientInput() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblWeightLossTracker = new JLabel("Weight Loss Tracker");
+		lblWeightLossTracker.setBounds(148, 6, 133, 16);
+		contentPane.add(lblWeightLossTracker);
+	}
+}
